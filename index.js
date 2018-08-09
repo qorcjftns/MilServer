d5057926a2cd7bd4096e6b5453d89bf30bbab5e0
qorcjftns <qorcjftns2@gmail.com>
2018-08-09 19:03:57 +0900

    Implementing Kakao API

--- index.js
+++ index.js
@@ -11,6 +11,23 @@ app.get("/", (req, res) => {
 	res.sendFile(__dirname + '/htmls/index.html');
 });
 
+app.get("/keyboard", (req, res) => {
+	var data = {
+		'type': 'buttons',
+		'buttons': ['과일', '채소', '정보']
+	};
+	res.json(data);
+});
+
+app.get("/message", (req, res) => {
+	var data = {
+		'type': 'buttons',
+		'buttons': ['과일', '채소', '정보']
+	};
+	res.json(data);
+});
+
+
 io.on('connection', function(socket) {
 	socket.on('chat message', function(msg) {
 		io.emit('chat message', msg);
@@ -20,3 +37,6 @@ io.on('connection', function(socket) {
 server.listen(PORT, function() {
 	console.log('listening on *:' + PORT);
 });
+
+const kakao_keyboard = require('./api/kakao/keyboard');
+const kakao_message = require('./api/kakao/message');
