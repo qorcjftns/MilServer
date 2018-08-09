d3fef8909845ddec8a9329538657acf8233d3916
qorcjftns <qorcjftns2@gmail.com>
2018-08-09 18:16:08 +0900

    simple chat test

--- index.js
+++ index.js
@@ -3,8 +3,18 @@ const express 	= require("express");
 const app 	= express();
 const PORT 	= process.env.PORT;
 
+// Socket.io
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
 app.get("/", (req, res) => {
 	res.send({hello: "world"});
+});
+
+io.on('connection', function(socket) {
+	socket.on('chat message', function(msg) {
+		io.emit('chat message', msg);
+	});
 });
 
 app.listen(PORT);
